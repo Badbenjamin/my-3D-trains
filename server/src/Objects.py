5679f02e80056dc3919df46f9fbf05aa2ec698a1
ben mccarthy <bmccarthy90@gmail.com>
2024-10-10 19:28:00 -0400

    filtering and arrival time working, data passed through html

--- server/src/Objects.py
+++ server/src/Objects.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from google.transit import gtfs_realtime_pb2
 
 from models import Station
-
+import pprint
 current_time = datetime.now()
 
 # convert 10 digit POSIX timestamp used in feed to readable format
@@ -20,6 +20,28 @@ def convert_seconds(seconds):
 def time_difference(first_time, second_time):
     detla_time = second_time - first_time
     return detla_time
+
+def trains_to_objects(train_list):
+        train_object_list = []
+        for train in train_list:
+            new_schedule = []
+            for stop in train.trip_update.stop_time_update:
+                new_stop = Stop(
+                    arrival= stop.arrival.time,
+                    departure= stop.departure.time,
+                    stop_id= stop.stop_id
+                )
+                new_schedule.append(new_stop)
+            
+            new_train = Train(
+                trip_id= train.trip_update.trip.trip_id,
+                start_time= train.trip_update.trip.start_time,
+                start_date= train.trip_update.trip.start_date,
+                route_id= train.trip_update.trip.route_id,
+                schedule= new_schedule
+            )
+            train_object_list.append(new_train)
+        return train_object_list
 
 class Journey:
 
@@ -57,7 +79,42 @@ class Train:
         self.start_date = start_date
         self.route_id = route_id
         self.schedule = schedule
+
+    def last_stop(self):
+        return self.schedule[0]
     
+    def next_stop(self):
+        return self.schedule[1]
+
+    def arrival_time(self, station_gtfs_id):
+        for stop in self.schedule:
+            if stop.stop_id[:-1] == station_gtfs_id:
+                return stop.arrival
+            
+    def route(self):
+        return self.route_id
+    
+    def direction(self):
+        return self.schedule[0].stop_id[-1]
+            
+    def current_location(self):
+        location = {
+            "last_stop" : self.schedule[0].stop_id,
+            "last_stop_departure" : self.schedule[0].departure,
+            "next_stop" : self.schedule[1].stop_id,
+            "next_stop_arrival" : self.schedule[1].arrival,
+            "length_of_trip" : convert_seconds(self.schedule[1].arrival - self.schedule[0].departure)
+        }
+        return location
+
+            
+    # work on this later
+    def number_of_stops(self, start_station_gtfs_id, end_station_gtfs_id):
+        pass
+
+    def time_between_stops(self, start_station_gtfs_id, end_station_gtfs_id):
+        pass
+
     def __repr__(self):
         return f'<Train {self.trip_id}>'
 
@@ -69,7 +126,7 @@ class Stop:
         self.stop_id = stop_id
 
     def __repr__(self):
-        return f'<Stop {self.stop_id}>'
+        return f'<Stop {self.stop_id} {str(convert_timestamp(self.arrival))[11:-3]}>'
 
 
 # accepts journey object as arg
@@ -150,33 +207,47 @@ class TrainData:
                         for stop in train.trip_update.stop_time_update:
                             if stop.stop_id[:-1] == start_station_id and time_difference(current_time, convert_timestamp(stop.arrival.time)) > convert_seconds(30):
                                 filtered_trains.append(train)
-        return filtered_trains
+        return trains_to_objects(filtered_trains)
     
-    def trains_to_objects(self, train_list):
-        train_object_list = []
-        for train in train_list:
-            new_schedule = []
-            for stop in train.trip_update.stop_time_update:
-                new_stop = Stop(
-                    arrival= stop.arrival.time,
-                    departure= stop.departure.time,
-                    stop_id= stop.stop_id
-                )
-                new_schedule.append(new_stop)
-            
-            new_train = Train(
-                trip_id= train.trip_update.trip.trip_id,
-                start_time= train.trip_update.trip.start_time,
-                start_date= train.trip_update.trip.start_date,
-                route_id= train.trip_update.trip.route_id,
-                schedule= new_schedule
-            )
-            train_object_list.append(new_train)
-        return train_object_list
     
+    # takes filtered list of trains and sorts by arrival time at destination
     def sort_trains_by_arrival_at_destination(self):
+        end_station_id = self.journey_object.end_station.gtfs_stop_id
         trains_with_arrival = []
-        pass
+        for train in self.filter_trains_for_stations_direction_current():
+            arrival_train = {"train" : train, "dest_arrival_time" : None}
+            for stop in train.schedule:
+                if stop.stop_id[:-1] == end_station_id:
+                    arrival_train['dest_arrival_time'] = stop.arrival
+            trains_with_arrival.append(arrival_train)
+        trains_by_dest_arrival = sorted(trains_with_arrival, key=lambda d: d['dest_arrival_time'])
+        # print(trains_by_dest_arrival)
+        return [arrival_train["train"] for arrival_train in trains_by_dest_arrival]
+       
+    def format_for_react(self, journey_object):
+        trains_for_react = []
+        
+        for train in self.sort_trains_by_arrival_at_destination():
+            train_for_react = {
+                "train_id" : train.trip_id,
+                "start_station" : journey_object.start_station.stop_name,
+                "start_station_arrival" : str(convert_timestamp(train.arrival_time(journey_object.start_station.gtfs_stop_id))),
+                "end_station" : journey_object.end_station.stop_name,
+                "end_station_arrival" : str(convert_timestamp(train.arrival_time(journey_object.end_station.gtfs_stop_id))),
+                "transfer_station" : None,
+                "route" : train.route(),
+                "direction_label" : None
+            }
+            if train.direction() == "N":
+                train_for_react['direction_label'] = journey_object.start_station.north_direction_label
+            if train.direction() == "S":
+                train_for_react['direction_label'] = journey_object.start_station.south_direction_label
+            trains_for_react.append(train_for_react)
+            # print(train_for_react['start_station_arrival'])
+        return trains_for_react
+
+
+        
             
 
     def __repr__(self):
@@ -188,6 +259,17 @@ if __name__ == "__main__":
     with app.app_context():
         new_journey = Journey(175, 178)
         new_data = TrainData(new_journey)
+        # print(new_journey.start_station.gtfs_stop_id)
+        # sorted_trains = new_data.sort_trains_by_arrival_at_destination()
+        # for train in sorted_trains:
+        #     print(train.arrival_time(train.arrival_time(new_journey.end_station.gtfs_stop_id)))
+
+        trains_for_react = new_data.format_for_react(new_journey)
+        # for train in trains_for_react:
+        #     print(train["start_station_arrival"])
+
         # for train in new_data.filter_trains_for_stations_direction_current():
         #     print("fsd", train)
-        print(new_data.trains_to_objects(new_data.filter_trains_for_stations_direction_current()))
+        # t1 = trains_to_objects(new_data.filter_trains_for_stations_direction_current())[0]
+        # # print(t1.schedule)
+        # print(t1.current_location())
--- server/src/app.py
+++ server/src/app.py
@@ -15,7 +15,7 @@ from google.transit import gtfs_realtime_pb2
 # from Rider import Rider
 
 from models import Station, Endpoint, StationEndpoint, Rider, Commute
-from Objects import Journey
+from Objects import Journey, TrainData, Train, Stop
 
 
 
@@ -24,7 +24,5 @@ ct = datetime.now()
 @app.route('/api/plan_trip/<string:start_station_id>/<string:end_station_id>')
 def plan_trip(start_station_id, end_station_id):
     new_journey = Journey(start_station_id, end_station_id)
-    print(new_journey.start_station)
-    print(new_journey.end_station)
-    # return new_journey.start_station.to_dict(), 200
-    return {'start' : new_journey.start_station.stop_name, 'end' : new_journey.end_station.stop_name }, 200
+    new_data = TrainData(new_journey)
+    return new_data.format_for_react(new_journey), 200
