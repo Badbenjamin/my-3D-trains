9af27a7ec4bebd565af9001f91ee940d263e75f9
ben mccarthy <bmccarthy90@gmail.com>
2024-10-15 19:16:24 -0400

    subway map loaded and working

--- client/src/SubwayMap.jsx
+++ client/src/SubwayMap.jsx
@@ -1,27 +1,49 @@
-import { useFrame, extend, useThree } from "@react-three/fiber"
+import { useFrame} from "@react-three/fiber"
 import { useRef } from 'react'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {MapControls, Html} from '@react-three/drei'
+import {Perf} from 'r3f-perf'
+import * as THREE from 'three'
+import './index.css'
+import { useLoader } from "@react-three/fiber"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-extend({OrbitControls}) 
+
 
 function SubwayMap(){
 
-    const {camera, gl} = useThree()
+    const map = useLoader(GLTFLoader, './subway_map.glb')
+    console.log(map)
+    const torusRef = useRef()
     const cubeRef = useRef()
 
-
+    const clock = new THREE.Clock()
+    
+    
 
     useFrame((state, delta) => {
-        cubeRef.current.rotation.y += delta
+        // const elapsedTime = clock.getElapsedTime()
+        // // console.log(elapsedTime)
+        // torusRef.current.rotation.y += delta
+        // cubeRef.current.position.y = Math.sin(elapsedTime) * 4
     })
 
     return(
         <>
-            <orbitControls args={[camera, gl.domElement]}/>
-            <mesh ref={cubeRef} >
+            <Perf position="top-left" />
+            <MapControls/>
+            <ambientLight intensity={1.5} />
+            <primitive object={map.scene}/>
+            {/* <mesh ref={torusRef} >
                 <torusKnotGeometry args={[1, 0.4, 64, 8]} />
                 <meshNormalMaterial />
+                <Html distanceFactor={8} wrapperClass="label" position={[1,1,1]}>TorusKnot</Html>
             </mesh>
+            <mesh ref={cubeRef} >
+                <boxGeometry />
+                <meshNormalMaterial />
+                <Html distanceFactor={8}  wrapperClass="label" position={[1,1,1]}>Cube</Html>
+            </mesh> */}
+            
         </>
         
     )
