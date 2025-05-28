04c87380be01792e9609a97da49cad69b14de8c5
ben mccarthy <bmccarthy90@gmail.com>
2025-05-28 19:32:20 -0400

    route logos appear in station text component

--- client/src/StationText.jsx
+++ client/src/StationText.jsx
@@ -3,15 +3,40 @@ import { useFrame } from "@react-three/fiber";
 
 
 import A from '../public/ICONS/A.png';
+import C from '../public/ICONS/C.png';
+import E from '../public/ICONS/E.png';
+
+import One from '../public/ICONS/1.png';
+import Two from '../public/ICONS/2.png';
+import Three from '../public/ICONS/3.png';
+
+import Four from '../public/ICONS/4.png';
+import Five from '../public/ICONS/5.png';
+import Six from '../public/ICONS/6.png';
+
+import Seven from '../public/ICONS/7.png';
+
+import B from '../public/ICONS/B.png';
+import D from '../public/ICONS/D.png';
+import F from '../public/ICONS/F.png';
+import M from '../public/ICONS/M.png';
+
+import J from '../public/ICONS/J.png';
+import Z from '../public/ICONS/Z.png';
+
+import L from '../public/ICONS/L.png';
 
 import './App.css'
 
 export default function StationText({handleStationClick, position, daytime_routes, name, status, gtfs_stop_id, alphaLevel}){
-    // console.log('dist sta', distance)
+    console.log('dist sta', daytime_routes)
 
-    let iconImage = <Image url={A}/>
-    console.log(A)
-    // let opacityLevel = alphaLevel
+    let iconImageArray = []
+    let routesArray = daytime_routes.split(" ").map((route)=>{
+        iconImageArray.push(<img className="route_icon" src={`../public/ICONS/${route}.png`}/>)
+    })
+
+
 
     function handleClick(e){
         handleStationClick(gtfs_stop_id)
@@ -23,12 +48,14 @@ export default function StationText({handleStationClick, position, daytime_route
         <>
             {/* {iconImage} */}
             {status ? <Html style={{opacity : alphaLevel}} wrapperClass="station_label" distanceFactor={5} center={true} position={position}>{<button className="station-html-button-text" onClick={handleClick}>
-                {name + " " + daytime_routes}</button>}
-                <img className="route_icon" src={A}/>
+                {name}</button>}
+                {/* <img className="route_icon" src={A}/> */}
+                {iconImageArray}
                 </Html> : <></>}
-            {/* {<Html position={position}>
-                <img className="route_icon" src={A}/>
-            </Html>} */}
+            {/* {<Html position={position}> */}
+                {/* <img className="route_icon" src={A}/> */}
+                {/* {array} */}
+            {/* </Html>} */}
         </>
     )
 }
