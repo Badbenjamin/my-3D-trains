ee68367afb195cc863bf6f7ae2a23b7da9abb555
ben mccarthy <bmccarthy90@gmail.com>
2025-09-08 14:39:50 -0400

    css formatting for single leg trip in LegInfo is complete

--- client/src/LegInfo.jsx
+++ client/src/LegInfo.jsx
@@ -7,24 +7,6 @@ function LegInfo({leg, type}){
     let routeIcon = <img className='route_icon_leg_info' src={`../public/ICONS/${leg.route}.png`}/>
     if (type == 'single-leg'){
         return(
-            // OLD
-            // <div className="leg-info-grid-cointainer">
-            //     <div className="start-station-info">
-            //         <div className="top">{leg.start_station}</div>
-            //         <div className="middle">{leg.direction_label} {routeIcon}</div>
-                    // <div className="bottom">Departs {leg.start_station_departure}</div><>{leg.first_six_trains}</>
-            //     </div>
-            //     <div className="middle-info">
-            //         <div className="top">{leg.number_of_stops} Stops</div>
-            //         <div className="middle">→</div>
-            //         <div className="bottom">{leg.trip_time} Minutes</div>
-            //     </div>
-            //     <div className="end-station-info">
-            //         <div className="top">{leg.end_station}</div>
-            //         <div className="middle">t or d</div>
-            //         <div className="bottom">Arrives {leg.end_station_arrival}</div>
-            //     </div>
-            // </div>
             <div>
                 <div className="start-station-info">
                     <div className="station">{leg.start_station}</div>
@@ -33,8 +15,10 @@ function LegInfo({leg, type}){
                 </div>
                 <div className="middle-info">
                     <div className='arrow-icon'>⬇</div>
-                    <div className="number-of-stops">{leg.number_of_stops} Stops</div>
-                    <div className="trip-time">{leg.trip_time} Minutes</div>
+                    <div className='stops-and-time'>
+                        <div className="number-of-stops">{leg.number_of_stops} Stops</div>
+                        <div className="trip-time">{leg.trip_time} Minutes</div>
+                    </div>
                 </div>
                 <div className="end-station-info">
                     <div className="station">{leg.end_station}</div>
