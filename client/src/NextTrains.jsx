0771fe1469760b18476318f49d88ed2619897eb3
ben mccarthy <bmccarthy90@gmail.com>
2025-10-08 15:22:32 -0400

    next trains departure time display calculated in browser from timestamp now

--- client/src/NextTrains.jsx
+++ client/src/NextTrains.jsx
@@ -2,13 +2,23 @@ import './Component.css'
 
 export default function NextTrains({tripInfoIndex, tripInfo, setTripInfoIndex}){
 
+    // console.log('depart', trip[0].start_station_departure, trip[0])
+
     let arrivalsButtons = []
     if (tripInfo.length > 0){
         arrivalsButtons = tripInfo.map((trip, index)=>{
+            //  console.log('depart', trip[0].start_station_departure, trip[0])
+
+            let departureTime = new Date(trip[0].start_station_departure_ts * 1000)
+            let departureTimeString = departureTime.toLocaleTimeString('en-US', { 
+                hour: 'numeric', 
+                minute: '2-digit',
+                hour12: true 
+            }) 
             if (index != tripInfoIndex){
-                return <button className='next-train-button' onClick={()=>{handleButtonClick(index)}}>{<img className="route_icon_next_train" src={`../ICONS/${trip[0].route}.png`}/>}{" " + trip[0].start_station_departure + " "}</button>
+                return <button className='next-train-button' onClick={()=>{handleButtonClick(index)}}>{<img className="route_icon_next_train" src={`../ICONS/${trip[0].route}.png`}/>}{" " + departureTimeString + " "}</button>
             } else {
-                return <button className='next-train-button-selected' onClick={()=>{handleButtonClick(index)}}>{<img className="route_icon_next_train" src={`../ICONS/${trip[0].route}.png`}/>}{" " + trip[0].start_station_departure + " "}</button>
+                return <button className='next-train-button-selected' onClick={()=>{handleButtonClick(index)}}>{<img className="route_icon_next_train" src={`../ICONS/${trip[0].route}.png`}/>}{" " + departureTimeString + " "}</button>
             }  
         })
     }
